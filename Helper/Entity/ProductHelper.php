dc5e3f35c021df79cb7b0a1824e62c549d907f5e
rachel-trott <rachel.trott@algolia.com>
2022-07-15 16:43:04 -0500

    MAGE-327 - Add FilterOnly Option in facet settings and modify pre indexing to include filterOnly attributes in attributesForFaceting

--- Helper/Entity/ProductHelper.php
+++ Helper/Entity/ProductHelper.php
@@ -1000,9 +1000,14 @@ class ProductHelper
                 }
             } else {
                 $attribute = $facet['attribute'];
-                if (array_key_exists('searchable', $facet) && $facet['searchable'] === '1') {
-                    $attribute = 'searchable(' . $attribute . ')';
+                if (array_key_exists('searchable', $facet)) {
+                    if ($facet['searchable'] === '1') {
+                        $attribute = 'searchable(' . $attribute . ')';
+                    } elseif ($facet['searchable'] === '3') {
+                        $attribute = 'filterOnly(' . $attribute . ')';
+                    }
                 }
+
 
                 $attributesForFaceting[] = $attribute;
             }
